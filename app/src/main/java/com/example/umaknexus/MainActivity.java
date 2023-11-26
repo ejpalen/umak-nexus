04be326f1d61eca5f94daba399bc42cd806d694b
Ej Palen <johnejpalen13@gmail.com>
2023-11-26 16:54:26 +0800

    Initialized the UMak Nexus App

---
+++ app/src/main/java/com/example/umaknexus/MainActivity.java
@@ -0,0 +1,28 @@
+package com.example.umaknexus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button getStartedBtn = findViewById(R.id.getStarted_btn);
+
+        getStartedBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent signInIntent = new Intent(MainActivity.this, Onboarding_Signin.class);
+                startActivity(signInIntent);
+            }
+        });
+
+    }
+}
