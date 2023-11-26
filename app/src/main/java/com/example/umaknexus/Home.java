dce67dd8f965d59f559fd7eccf4cf5133efdf1db
Ej Palen <johnejpalen13@gmail.com>
2023-11-26 19:45:44 +0800

    Created the Bottom Navigation

---
+++ app/src/main/java/com/example/umaknexus/Home.java
@@ -0,0 +1,43 @@
+package com.example.umaknexus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+public class Home extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_home);
+//        bottomNavigationView.setOnItemSelectedListener(item -> {
+//            switch (item.getItemId()) {
+//                case R.id.bottom_home:
+//                    return true;
+//                case R.id.bottom_shop:
+//                    startActivity(new Intent(getApplicationContext(),Home.class));
+//                    finish();
+//                    return true;
+//                case R.id.bottom_cart:
+//                    startActivity(new Intent(getApplicationContext(),Home.class));
+//                    finish();
+//                    return true;
+//                case R.id.bottom_notifications:
+//                    startActivity(new Intent(getApplicationContext(),Home.class));
+//                    finish();
+//                    return true;
+//                case R.id.bottom_profile:
+//                    startActivity(new Intent(getApplicationContext(),Home.class));
+//                    finish();
+//                    return true;
+//            }
+//            return false;
+//        });
+    }
+}
--- app/src/main/java/com/example/umaknexus/Onboarding_Signin.java
+++ app/src/main/java/com/example/umaknexus/Onboarding_Signin.java
@@ -2,6 +2,7 @@ package com.example.umaknexus;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 public class Onboarding_Signin extends AppCompatActivity {
@@ -10,5 +11,7 @@ public class Onboarding_Signin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_onboarding_signin);
+
+        startActivity(new Intent(getApplicationContext(), Home.class));
     }
 }
