ab93fa5dfefe452c32e8716e10acb4c28a3af1b2
Aaron Planta <aplanta.a12137285@umak.edu.ph>
2023-12-02 03:40:11 +0800

    Wishlist design code updated

---
+++ app/src/main/java/com/example/umaknexus/Wishlist.java
@@ -0,0 +1,66 @@
+package com.example.umaknexus;
+
+import android.content.Intent;
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.GridLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Wishlist extends AppCompatActivity {
+
+    RecyclerView productWishlist;
+    RecyclerView.LayoutManager wishlistProductLayoutManager;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_wishlist);
+
+        productWishlist=findViewById(R.id.productWishlist);
+
+        List<Products> wishlistItems=new ArrayList<Products>();
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+        wishlistItems.add(new Products("UNIFORM (FEMALE)", "$300.00",R.drawable.unif_sample));
+
+        wishlistProductLayoutManager = new GridLayoutManager(this, 2);
+        productWishlist.setLayoutManager(wishlistProductLayoutManager);
+        productWishlist.setAdapter(new shopProductsAdapter(getApplication(), wishlistItems));
+
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
+        bottomNavigationView.setSelectedItemId(R.id.bottom_shop);
+        bottomNavigationView.setOnItemSelectedListener(item -> {
+            if (item.getItemId() == R.id.bottom_home) {
+                startActivity(new Intent(getApplicationContext(), Home.class));
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_shop) {
+                return true;
+            } else if (item.getItemId() == R.id.bottom_cart) {
+                startActivity(new Intent(getApplicationContext(), Cart_Page.class));
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_notifications) {
+                startActivity(new Intent(getApplicationContext(), Home.class));
+                finish();
+                return true;
+            } else if (item.getItemId() == R.id.bottom_profile) {
+                startActivity(new Intent(getApplicationContext(), Home.class));
+                finish();
+                return true;
+            }
+
+            return false;
+        });
+    }
+}
