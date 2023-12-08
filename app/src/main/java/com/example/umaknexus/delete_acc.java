3198659cdbda39b53c3b16ade49f70f29f0ca691
DEBBIE TOLIBAS <dtolibas.k11935758@umak.edu.ph>
2023-12-08 15:51:44 +0000

    all design finished, fixed minor issues regarding margin and alignment. added delete page

---
+++ app/src/main/java/com/example/umaknexus/delete_acc.java
@@ -0,0 +1,14 @@
+package com.example.umaknexus;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class delete_acc extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_delete_acc);
+    }
+}
