46da59406a1b4c95e2506b1f4781dc7cefa3578d
solomon <kurobaneyashiro@gmail.com>
2023-12-08 16:03:49 +0800

    Added Profile, Profile Edit, Order History
    To do:
    Functions, profile edit page, order history polishing

---
+++ app/src/main/java/com/example/umaknexus/OrderHistory_Holder.java
@@ -0,0 +1,23 @@
+package com.example.umaknexus;
+
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class OrderHistory_Holder extends RecyclerView.ViewHolder {
+    TextView order_number, order_status, item_name, item_price, total_price;
+
+    public OrderHistory_Holder(@NonNull View itemView) {
+        super(itemView);
+
+        order_number = itemView.findViewById(R.id.order_number);
+        order_status = itemView.findViewById(R.id.order_status);
+        item_name = itemView.findViewById(R.id.item_name);
+        item_price = itemView.findViewById(R.id.item_price);
+        total_price = itemView.findViewById(R.id.total_price);
+
+    }
+}
