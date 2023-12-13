5cba7e5d951dd7aa5acf7d11aac8ea1a724cc1b2
Julina Kay Gacura <jgacura.k11937320@umak.edu.ph>
2023-12-13 19:02:41 +0800

    BASTA MADAME

--- app/src/main/java/com/example/umaknexus/Cart_Item.java
+++ app/src/main/java/com/example/umaknexus/Cart_Item.java
@@ -1,15 +1,18 @@
 package com.example.umaknexus;
 
 public class Cart_Item {
-    String prodName, prodPrice, qty_item;
-    int img_product, delete_btn;
+    String prodName, prodPrice, qty_item, img_product;
+    int delete_btn;
 
-    public Cart_Item(String prodName, String prodPrice, String qty_item, int img_product, int delete_btn) {
+    public Cart_Item(String prodName, String prodPrice, String qty_item, String img_product, int delete_btn) {
         this.prodName = prodName;
         this.prodPrice = prodPrice;
         this.qty_item = qty_item;
         this.img_product = img_product;
         this.delete_btn = delete_btn;
+    }
+
+    public static void clear() {
     }
 
     public String getProdName() {
@@ -36,13 +39,6 @@ public class Cart_Item {
         this.qty_item = qty_item;
     }
 
-    public int getImg_product() {
-        return img_product;
-    }
-
-    public void setImg_product(int img_product) {
-        this.img_product = img_product;
-    }
 
     public int getDelete_btn() {
         return delete_btn;
@@ -52,4 +48,10 @@ public class Cart_Item {
         this.delete_btn = delete_btn;
     }
 
+    public String getImg_product() {
+        return img_product;
+    }
+    public void setImg_product(String icon) {
+        this.img_product = img_product;
+    }
 }
--- app/src/main/java/com/example/umaknexus/ProductPage.java
+++ app/src/main/java/com/example/umaknexus/ProductPage.java
@@ -3,6 +3,7 @@ package com.example.umaknexus;
 import android.os.Bundle;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.FrameLayout;
@@ -12,12 +13,18 @@ import android.widget.Toast;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import com.google.android.gms.tasks.OnCompleteListener;
 import com.google.android.gms.tasks.OnFailureListener;
 import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.firestore.CollectionReference;
 import com.google.firebase.firestore.DocumentReference;
+import com.google.firebase.firestore.DocumentSnapshot;
 import com.google.firebase.firestore.FirebaseFirestore;
+import com.google.firebase.firestore.WriteBatch;
 
 import org.w3c.dom.Text;
 
@@ -25,32 +32,42 @@ import java.util.HashMap;
 import java.util.Map;
 
 public class ProductPage extends AppCompatActivity {
-    Button btn_s, btn_m, btn_l, btn_addtocart, btn_addtowishlist;
     FirebaseFirestore db;
     FirebaseAuth auth;
+    private TextView productQtyTextView;
+    private int productQty = 1;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_productpage);
 
-//        public void onSizeButtonClick(View view){
-////            Reset background color for all buttons
-//            Button btn_addtocart = findViewById(R.id.btn_addtocart);
-            Button btn_addtowishlist = findViewById(R.id.edit_profile);
-            db = FirebaseFirestore.getInstance();
-            auth = FirebaseAuth.getInstance();
+        TextView productName = findViewById(R.id.prodName);
+        TextView productQty = findViewById(R.id.qty_item);
+        TextView productPrice = findViewById(R.id.price);
+        Button addQty = findViewById(R.id.btn_add);
+        Button subtractQty = findViewById(R.id.btn_subtract);
+        Button btn_addtocart = findViewById(R.id.btn_addtocart);
+        Button btn_addtowishlist = findViewById(R.id.btn_addtowishlist);
+        db = FirebaseFirestore.getInstance();
+        auth = FirebaseAuth.getInstance();
 
-//
-//            btn_addtocart.setSelected(R.id.btn_addtocart);
-//            btn_addtowishlist.setSelected(R.id.btn_addtowishlist);
-//
-//            // Set background color for the clicked button
-//            Button clickedButton = (Button) view;
-//            clickedButton.setBackgroundResource(R.drawable.item_selector_selected);
-//            String buttonText = clickedButton.getText().toString();
-//
-//            // Handle the button click logic here
-//        }
+        // Set initial quantity in TextView
+        productQtyTextView.setText(String.valueOf(productQty));
+
+        // Set click listeners for add and subtract buttons
+        addQty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                incrementQuantity();
+            }
+        });
+
+        subtractQty.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                decrementQuantity();
+            }
+        });
 
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottomNavigation);
         bottomNavigationView.setSelectedItemId(R.id.bottom_shop);
@@ -76,10 +93,9 @@ public class ProductPage extends AppCompatActivity {
             }
 
             return false;
-       });
+        });
 
         FrameLayout back_btn = findViewById(R.id.backButtonFrame);
-
         back_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -88,6 +104,39 @@ public class ProductPage extends AppCompatActivity {
             }
         });
 
+        btn_addtocart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                TextView productName = findViewById(R.id.prodName);
+                TextView productQty = findViewById(R.id.qty_item);
+                TextView productPrice = findViewById(R.id.price);
+
+                String product = productName.getText().toString();
+                int quantity = Integer.parseInt(productQty.getText().toString());
+                String price = productPrice.getText().toString();
+
+                Map<String, Object> cartData = new HashMap<>();
+                cartData.put("product_name", product);
+                cartData.put("product_quantity", quantity);
+                cartData.put("product_subtotal", price);
+
+
+                Map<String, Object> cartProducts = new HashMap<>();
+                cartProducts.put("products", cartData);
+
+                db.collection("cart").add(cartProducts).addOnSuccessListener(new OnSuccessListener<DocumentReference>() {
+                    @Override
+                    public void onSuccess(DocumentReference documentReference) {
+                        Toast.makeText(getApplicationContext(), "Product added to cart.", Toast.LENGTH_SHORT).show();
+                    }
+                }).addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(getApplicationContext(), "Error adding product: " + e.getMessage(), Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        });
         btn_addtowishlist.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -124,6 +173,21 @@ public class ProductPage extends AppCompatActivity {
                 finish();
             }
         });
-
     }
+        private void incrementQuantity() {
+            productQty++;
+            updateQuantityTextView();
+        }
+
+        private void decrementQuantity() {
+            if (productQty > 1) {
+                productQty--;
+                updateQuantityTextView();
+            }
+            else {
+            }
+        }
+        private void updateQuantityTextView() {
+            productQtyTextView.setText(String.valueOf(productQty));
+        }
 }
