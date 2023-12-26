ec0d967fd5c8370d4577722b436b9fcf8eaa71d4
Julina Kay Gacura <jgacura.k11937320@umak.edu.ph>
2023-12-26 08:51:24 +0800

    CART PAGE:

--- app/src/main/java/com/example/umaknexus/Order_Confirmation.java
+++ app/src/main/java/com/example/umaknexus/Order_Confirmation.java
@@ -1,25 +1,77 @@
 package com.example.umaknexus;
 
-import androidx.appcompat.app.AppCompatActivity;
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
-import com.google.firebase.auth.FirebaseAuth;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
 
-public class Order_Confirmation extends AppCompatActivity{
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.firestore.DocumentSnapshot;
+import com.google.firebase.firestore.FirebaseFirestore;
+
+import java.util.Locale;
+
+public class Order_Confirmation extends AppCompatActivity {
+    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_orderconfirmation);
 
+        // Retrieve order ID from the intent
+        String orderId = getIntent().getStringExtra("order_id");
+
+        // Update TextViews with the retrieved values
+        TextView orderStatusTextView = findViewById(R.id.orderStatus);
+        TextView orderTitleTextView = findViewById(R.id.orderTitle);
+        TextView nameTextView = findViewById(R.id.name);
+        TextView dateTextView = findViewById(R.id.date);
+        TextView totalAmountTextView = findViewById(R.id.total);
+
+        // Fetch total amount from Firestore
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+        db.collection("orders")
+                .document(orderId)
+                .get()
+                .addOnCompleteListener(new OnCompleteListener<DocumentSnapshot>() {
+                    @Override
+                    public void onComplete(@NonNull Task<DocumentSnapshot> task) {
+                        if (task.isSuccessful()) {
+                            DocumentSnapshot document = task.getResult();
+                            if (document.exists()) {
+                                // Retrieve the total amount from the document
+                                double totalAmount = document.getDouble("total_amount");
+
+                                // Retrieve user's name and purchase date from the document
+                                String userName = document.getString("user_name");
+                                String purchaseDate = document.getString("purchase_date");
+
+                                // Update TextViews with the retrieved values
+                                orderStatusTextView.setText(getString(R.string.order_status_message));
+                                orderTitleTextView.setText(getString(R.string.order_number_message, orderId));
+                                nameTextView.setText(userName);
+                                dateTextView.setText(purchaseDate);
+
+                                // Set the total amount directly
+                                totalAmountTextView.setText(getString(R.string.total_amount_message, String.format(Locale.getDefault(), "%.2f", totalAmount)));
+                            } else {
+                                // Document does not exist
+                                // Handle the case when the document is not found
+                            }
+                        } else {
+                            // Handle errors
+                        }
+                    }
+                });
+
+        // Close button click listener
         Button close = findViewById(R.id.btn_close);
-        close.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                startActivity(new Intent(getApplicationContext(), Home.class));
-                finish();
-            }
+        close.setOnClickListener(view -> {
+            startActivity(new Intent(getApplicationContext(), Home.class));
+            finish();
         });
     }
 }
